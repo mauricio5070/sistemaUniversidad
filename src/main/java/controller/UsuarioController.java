d7bfd24c532c8140fccba1abb4aa0da6676f594c
JOSE MURILLO <josedmurilloc@gmail.com>
2021-02-09 18:09:28 -0600

    My Firsts Commit

---
+++ src/main/java/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import gestion.UsuarioGestion;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import model.Usuario;
+
+/**
+ *
+ * @author User
+ */
+@Named(value = "usuarioController")
+@SessionScoped
+public class UsuarioController extends Usuario implements Serializable {
+
+    /**
+     * Creates a new instance of UsuarioController
+     */
+    public UsuarioController() {
+    }
+
+    public String getUsuario() {
+        Usuario usuario = UsuarioGestion.getUsuario(this.getIdUsuario(), this.getPwUsuario());
+        if (usuario != null) {
+            this.setNombreUsuario(usuario.getNombreUsuario());
+            this.setIdRol(usuario.getIdRol());
+            return "principal.xhtml";
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+                    "Usuario o Contraseña incorrecta");
+            FacesContext.getCurrentInstance().addMessage("loginForm:clave", msg);
+            return "index.xhtml";
+        }
+    }
+}
